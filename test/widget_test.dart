c7757322ba70fc199d7c8ece626d1fc104788ade
Júlia Lamboglia <julia.conip@gmail.com>
2024-02-01 19:31:39 -0300

    ajuste do código

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:learning_again_flutter/main.dart';
+import 'package:learning_again_flutter/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
